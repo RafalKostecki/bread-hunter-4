9513a082ef63ce8fd6df127962d165465177f4fb
RafalKostecki <edward-12@o2.pl>
2019-02-08 21:22:24 +0100

    Create Characters component

--- src/components/controlPanel/Characters.js
+++ src/components/controlPanel/Characters.js
@@ -1,20 +1,43 @@
 import React from 'react';
 
+//Import images
+import kostekChar from '../../assets/images/kostekCharIcon.png';
+import marekChar from '../../assets/images/marekCharIcon.png';
+import matiChar from '../../assets/images/matiCharIcon.png';
+
+const characters = [
+    {
+        name: "Kostek",
+        pic: kostekChar
+    },
+    {
+        name: "Marek",
+        pic: marekChar
+    },
+    {
+        name: "Mati",
+        pic: matiChar
+    },
+    {
+        name: "Piotr",
+        pic: matiChar
+    }
+]
 
 const Characters = () => {
   return (
     <div role="presentation" className="characters">
         <div className="characters__char">
-            kostek
+            <img src={kostekChar} alt="Kostek" />
         </div>
         <div className="characters__char">
-            marek
+            <img src={kostekChar} alt="Kostek" />
         </div>
         <div className="characters__char">
-            mati
+            <img src={kostekChar} alt="Kostek" />
         </div>
         <div className="characters__char">
-            piotr
+            <img src={kostekChar} alt="Kostek" />
         </div>
     </div>
   )
