c2b43275331bf43842bb3e1dac23a6a8447703fb
RafalKostecki <edward-12@o2.pl>
2019-03-02 14:46:25 +0100

    Add PropTypes to BoardField component

--- src/components/board/BoardField.js
+++ src/components/board/BoardField.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 const BoardField = ({id}) => {
     return (
         <div className="boardField" id={id}>
-            {id}
+            
         </div>
     )
+}
+
+BoardField.propTypes = {
+    id: PropTypes.string.isRequired
 }
 
 export default BoardField;
