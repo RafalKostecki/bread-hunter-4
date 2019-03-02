44b94a6f301f046ecafe121861919745da7c941b
RafalKostecki <edward-12@o2.pl>
2019-03-02 11:05:59 +0100

    Create board matrix

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 //Import other components
 import BoardField from './BoardField';
 
 const Board = () => {
+    const boardConfig = {
+        x: 20,
+        y: 15,
+        fieldSize: 50
+    }
+
     const boardStyles = {
-        width: "1200px",
-        height: "850px",
+        width: `${boardConfig.x*boardConfig.fieldSize}px`,
+        height: `${boardConfig.y*boardConfig.fieldSize}px`,
         top: "0",
         left: "0"
     }
 
-    const boardFieldSize = 50;
-    const boardWidth = parseInt(boardStyles.width);
-    const boardHeight = parseInt(boardStyles.height);
-    console.log(boardWidth, boardHeight)
+    const boardMatrix = [];
 
-    const boardFieldQuantity = (boardWidth/boardFieldSize) * (boardHeight/boardFieldSize) + 6;
+    useEffect(()=> { //create boardMatrix
+        for (let y=0; y<boardConfig.y; y++) {
+            const yAxis = [];
+            for (let x=0; x<boardConfig.x; x++) {
+                yAxis.push(0)
+            }
+            boardMatrix.push(yAxis);
+        }
 
-    const boardFields = [...Array(boardFieldQuantity)].map((element, id) => {
-        return <BoardField 
-            key={id}
-            id={id}
-        />
+        console.log(boardMatrix);
     })
+
 
     return (
         <div className="boardWindow">
             <div className="board" style={boardStyles}>
-                {boardFields}
+                xdd
             </div>
         </div>
     )
